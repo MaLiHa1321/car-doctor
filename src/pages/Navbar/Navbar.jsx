6c9bebc1c1b7f703bc5c662eababfb73bf8c7407
MaLiHa1321 <malihasultana2001@gmail.com>
2023-10-22 20:49:55 +0600

    banner section are added

--- src/pages/Navbar/Navbar.jsx
+++ src/pages/Navbar/Navbar.jsx
@@ -1,26 +1,27 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 import logo from '../../assets/logo.svg';
 
 const Navbar = () => {
+
+    const navLink =<>
+    <NavLink to="/" className="mr-3 text-xl">Home</NavLink>
+    <NavLink to="/" className="mr-3 text-xl">About</NavLink>
+    <NavLink to="/" className="mr-3 text-xl">Service</NavLink>
+    <NavLink to="/" className="mr-3 text-xl">Blog</NavLink>
+    <NavLink to="/" className="mr-3 text-xl">Contact</NavLink>
+    </>
+
     return (
         <div>
-          <div className="navbar bg-base-100">
+          <div className="navbar bg-base-100 h-28">
   <div className="navbar-start">
     <div className="dropdown">
       <label tabIndex={0} className="btn btn-ghost lg:hidden">
         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
       </label>
       <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-        <li><a>Item 1</a></li>
-        <li>
-          <a>Parent</a>
-          <ul className="p-2">
-            <li><a>Submenu 1</a></li>
-            <li><a>Submenu 2</a></li>
-          </ul>
-        </li>
-        <li><a>Item 3</a></li>
+        {navLink}
       </ul>
     </div>
     <Link to='/' className="btn btn-ghost normal-case text-xl">
@@ -29,21 +30,11 @@ const Navbar = () => {
   </div>
   <div className="navbar-center hidden lg:flex">
     <ul className="menu menu-horizontal px-1">
-      <li><a>Item 1</a></li>
-      <li tabIndex={0}>
-        <details>
-          <summary>Parent</summary>
-          <ul className="p-2">
-            <li><a>Submenu 1</a></li>
-            <li><a>Submenu 2</a></li>
-          </ul>
-        </details>
-      </li>
-      <li><a>Item 3</a></li>
+    {navLink}
     </ul>
   </div>
   <div className="navbar-end">
-    <a className="btn">Button</a>
+    <button className="btn btn-outline btn-error">Appointment</button>
   </div>
 </div>
 
