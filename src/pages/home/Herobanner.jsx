794ef2cd533daf9dcf918882c7115ceaf029cee8
MaLiHa1321 <malihasultana2001@gmail.com>
2023-10-23 15:24:58 +0600

    stat section are added

---
+++ src/pages/home/Herobanner.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+
+const Herobanner = () => {
+    return (
+        <div className='mt-12 '>
+            <div className="stats shadow overflow-hidden flex flex-col md:flex-row w-10/12 h-2/4  md:p-5 mx-auto bg-black text-white">
+  
+  <div className="stat">
+    <div className="stat-figure text-secondary">
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-8 h-8 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
+    </div>
+    <div className="stat-title text-white">We are open monday-friday</div>
+    <div className="stat-value text-base md:text-2xl">7:00 am - 9:00 pm</div>
+  
+  </div>
+  
+  <div className="stat">
+    <div className="stat-figure text-secondary">
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-8 h-8 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 6V4m0 2a2 2 0 100 4m0-4a2 2 0 110 4m-6 8a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4m6 6v10m6-2a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4"></path></svg>
+    </div>
+    <div className="stat-title text-white">Have a question?</div>
+    <div className="stat-value text-base md:text-2xl">+2546 251 2658</div>
+  </div>
+  
+  <div className="stat">
+    <div className="stat-figure text-secondary">
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-8 h-8 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 8h14M5 8a2 2 0 110-4h14a2 2 0 110 4M5 8v10a2 2 0 002 2h10a2 2 0 002-2V8m-9 4h4"></path></svg>
+    </div>
+    <div className="stat-title text-white">Need a repair? our address</div>
+    <div className="stat-value text-base md:text-2xl">Liza Street, New York</div>
+  </div>
+  
+</div>
+            
+        </div>
+    );
+};
+
+export default Herobanner;
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Banner from './Banner';
 import About from './About';
 import Services from './Services';
+import Herobanner from './Herobanner';
 
 const Home = () => {
     return (
@@ -9,6 +10,7 @@ const Home = () => {
          <Banner></Banner>
          <About></About>
          <Services></Services>
+         <Herobanner></Herobanner>
         </div>
     );
 };
